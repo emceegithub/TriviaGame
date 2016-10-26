275b7b86d3e40ed4763b5551daf2cb283166c96d
jesseharold <jesseharold@gmail.com>
2016-10-25 19:52:52 -0700

    added a bunch of questions, no images yet

--- assets/javascript/questions-library.js
+++ assets/javascript/questions-library.js
@@ -64,5 +64,125 @@ var questLib = [
 	 	answers: ["He sold slaves", "He desserted from the Night's Watch", "He killed the king", "He was spying for the Targaryens"],
 	 	correctAnswer: 0,
 	 	image: "assets/images/questionImages/010.jpg"
+	},
+	{
+	 	question: "What is the birth order of the Lannister siblings?",
+	 	answers: ["Jaime, Cersei, Tyrion", "Tyrion, Cersei, Jaime", "Jaime, Tyrion, Cersei", "Cersei, Jaime, Tyrion"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "Which song was played to signal the slaughter at the Red Wedding?",
+	 	answers: ["The Gates of Hell", "The Shade of Asshai", "The Rains of Castamere", "The Lady Stoneheart"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "Who brought Catelyn Stark back to life?",
+	 	answers: ["Melisandre", "Thoros of Myr", "Jon Snow", "Beric Dondarrion"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "In the north, people ask for what to establish Guest Right?",
+	 	answers: ["bread and salt", "water and Wine", "pease porridge", "mac and cheese"],
+	 	correctAnswer: 0,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "What gods does Craster worship?",
+	 	answers: ["The Old Gods", "The Seven", "R'hllor", "The Others"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "Craster was Gilly's...",
+	 	answers: ["husband", "father", "neither", "both"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "At the beginning of the books, how long has it been since the last winter?",
+	 	answers: ["6 months", "1 year", "7 years", "35 years"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "How long before the start of the books did the last dragons die?",
+	 	answers: ["3 years", "39 years", "150 years", "300 years"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "Before Robert's Rebellion, Danaerys what what number in line to the Iron Throne?",
+	 	answers: ["1", "3", "6", "She was not in the line of succession"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "Who served the Freys pies made of their dead relatives?",
+	 	answers: ["Wyman Manderly", "Roose Bolton", "Maege Mormont", "Littlefinger"],
+	 	correctAnswer: 0,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "Who killed Jon Arryn, Hand of the King?",
+	 	answers: ["Cersei and Jaime Lannister", "Varys and Tyrion Lannister", "Littlefinger and Lysa Arryn", "Aerys Targaryen"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "What character is called 'Husband to Bears'?",
+	 	answers: ["Robb Stark", "Tormund Giantsbane", "Jorah Mormont", "Strong Belwas"],
+	 	correctAnswer: 1,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "How many direwolf pups did the Starks find with their dead monther?",
+	 	answers: ["Five", "Six", "Four", "Three"],
+	 	correctAnswer: 1,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "Who knighted The Hound?",
+	 	answers: ["King Robert", "King Aerys", "Ser Barristan Selmy", "No one"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "What color is Ghost, Jon's direwolf?",
+	 	answers: ["White", "Grey", "Black", "Red"],
+	 	correctAnswer: 0,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "Who was called 'The Beggar King'?",
+	 	answers: ["Robert Baratheon", "Viserys Targaryen", "Balon Greyjoy", "Renly Baratheon"],
+	 	correctAnswer: 1,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "Which of the following wasn't one of the five kings in the War of the Five Kings?",
+	 	answers: ["Balon Greyjoy", "Stannis Baratheon", "Viserys Targaryen", "Joffrey Baratheon"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "What was Daario Naharis before he joined Danaerys's cause?",
+	 	answers: ["A spy for the Lannisters", "A sellsword", "A mummer", "He never said"],
+	 	correctAnswer: 1,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "Who were Ned Stark's siblings?",
+	 	answers: ["Benjen, Ashara, and Peytr", "Robert and Jon", "Rickard, Lyanna, and Arya", "Brandon, Lyanna, and Benjen"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/01X.jpg"
+	},
+	{
+	 	question: "Who won the tourney thrown for Ned Stark's appointment as Hand?",
+	 	answers: ["Loras Tyrell", "Gregor Clegane", "Jaime Lannister", "Sandor Clegane"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/01X.jpg"
 	}
 ];
